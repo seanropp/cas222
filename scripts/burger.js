95434d8beb24c112de63679038a6cb1254c204db
seanropp <38111825+seanropp@users.noreply.github.com>
2018-05-05 01:41:06 -0700

    Ace in the Hole Project

---
+++ scripts/burger.js
@@ -0,0 +1,15 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+}
+
+
+/* function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+*/
